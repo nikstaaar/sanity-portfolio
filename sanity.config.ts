e825be44a74c89494a8b98eb8d5445cf561c3d4e
nikstaaar <niktjawalla922@gmail.com>
2024-05-28 22:30:59 +0200

    add project schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
+import schemas from "./sanity/schemas";
 
 export default defineConfig({
   projectId: "x08d25d5",
@@ -8,4 +9,5 @@ export default defineConfig({
   apiVersion: "2024-05-27",
   basePath: "/admin",
   plugins: [structureTool()],
+  schema: { types: schemas },
 });
