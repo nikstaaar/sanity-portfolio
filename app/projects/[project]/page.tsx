4bcc18d781131aaed983ec054f33e3feffb022e8
nikstaaar <niktjawalla922@gmail.com>
2024-06-04 23:31:19 +0200

    add content and image

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,5 +1,6 @@
 import { getProject } from "@/sanity/sanity-utils"
-
+import { PortableText } from "@portabletext/react"
+import Image from "next/image"
 type Props = {
   params:{ project: string }
 }
@@ -25,9 +26,16 @@ export default async function Project({params}:Props){
           view Project
         </a>
       </header>
-      {/*content goes here */}
-
-      {/*image goes here */}
+      <div className="text-lg text-gray-700 mt-5">
+        <PortableText value={project.content}/>
+      </div>
+      <Image 
+      src={project.image} 
+      alt={project.name}
+      width={1920}
+      height={1080}
+      className="mt-10 border-2 border-gray-700 object-cover rounded-xl"
+      />
     </div>
   </>
 }
