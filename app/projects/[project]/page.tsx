2224df08e5364927d70537c966391bf6764f9cd1
nikstaaar <niktjawalla922@gmail.com>
2024-06-04 23:20:05 +0200

    add project header

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -7,6 +7,28 @@ type Props = {
 export default async function Project({params}:Props){
   const slug = params.project
   const project = await getProject(slug)
-  return <div>{project.name}</div>
+  return <>
+    <div className="max-w-3xl mx-auto py-20">
+      <header className="flex item-center justify-between">
+        <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 
+        bg-clip-text text-transparent text-5xl drop-shadow font-extrabold"
+        >
+          {project.name}
+        </h1>
+        <a href={project.url}
+        title="View Project"
+        target="_blank"
+        rel="noopener norefferer"
+        className="bg-gra-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nwrap
+        hover:bg-pink-500 hover:text-pink-100 transition"
+        >
+          view Project
+        </a>
+      </header>
+      {/*content goes here */}
+
+      {/*image goes here */}
+    </div>
+  </>
 }
 
