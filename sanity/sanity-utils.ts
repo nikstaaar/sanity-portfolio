b1655a8e9f2d353bbf2e32ea0087dd73ab2a939c
nikstaaar <niktjawalla922@gmail.com>
2024-06-07 19:59:19 +0200

    add sanity pages schema and import

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -2,6 +2,7 @@ import { createClient, groq } from "next-sanity";
 import { Project } from "@/types/Project";
 
 import clientConfig from "./config/client-config";
+import { Page } from "@/types/Page";
 
 export async function getProjects(): Promise<Project[]> {
   return createClient(clientConfig).fetch(groq`*[_type == "project"]{
@@ -14,6 +15,21 @@ export async function getProject(slug: string): Promise<Project> {
     groq`*[_type == "project" && slug.current == $slug][0]{
     _id, _createdAt, name, "slug": slug.current, "image": image.asset->url, url, content
   }`,
+    { slug },
+  );
+}
+
+export async function getPages(): Promise<Page[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page"]
+    { _id, _createdAt, name, "slug": slug.current }`,
+  );
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == &slug[0]]
+    { _id, _createdAt, name, "slug": slug.current, content }`,
     { slug },
   );
 }
