718e92e0bcf6b8b8e65133cdf97ad8e9e49fcb2a
nikstaaar <niktjawalla922@gmail.com>
2024-06-07 19:36:17 +0200

    add page schema

---
+++ sanity/schemas/page-schema.ts
@@ -0,0 +1,24 @@
+import { kMaxLength } from "buffer";
+
+const page = {
+  name: "page",
+  title: "Pages",
+  type: "document",
+  fields: [
+    { name: "name", title: "Name", type: "string" },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title", maxLength: 96 },
+    },
+    {
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+  ],
+};
+
+export default page;
