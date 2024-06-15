4a14ac15814f4fc3033db488ff431e7f2287f978
nikstaaar <niktjawalla922@gmail.com>
2024-06-15 20:32:51 +0200

    add additional revalidate tags

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 
 import { getProjects } from "@/sanity/sanity-utils";
 
+export const revalidate = 30;
+
 export default async function Home() {
   const projects = await getProjects();
   return (
@@ -11,7 +13,7 @@ export default async function Home() {
         Hello I&apos;m
         <span
           className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 
-        bg-clip-text text-transparent"
+          bg-clip-text text-transparent"
         >
           {" "}
           Niklas
@@ -27,7 +29,8 @@ export default async function Home() {
           <Link
             href={`/projects/${project.slug}`}
             key={project._id}
-            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition"
+            className="border-2 border-gray-500 rounded-lg p-1 
+            hover:scale-105 hover:border-blue-500 transition"
           >
             {project.image && (
               <Image
@@ -39,8 +42,8 @@ export default async function Home() {
               />
             )}
             <div
-              className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 
-          bg-clip-text text-transparent"
+              className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 
+              to-purple-600 bg-clip-text text-transparent"
             >
               {project.name}
             </div>
